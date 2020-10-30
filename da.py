3f89212d374e262f87c01d8df0ce6c6124f10555
Bluecherry018 <38017209+Bluecherry018@users.noreply.github.com>
2020-10-30 10:54:36 -0300

    Update da.py
    Deposit account

--- da.py
+++ da.py
@@ -1,1 +1,12 @@
+from account import Account
+
+class DepositAccount (Account):
+    def withdraw (self, amount):
+        if self.balance < amount:
+            print('Insufficient funds')
+            return
+
+        self.balance -= amount
+        self.details()
+
 
